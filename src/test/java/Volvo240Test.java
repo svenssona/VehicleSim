c288f81b0e36444ef618a6e868b96ecb2b4203d4
RichardSvensson <ricsven@student.chalmers.se>
2021-11-09 09:19:05 +0100

    Add and updated tests for Volvo

--- src/test/java/Volvo240Test.java
+++ src/test/java/Volvo240Test.java
@@ -6,15 +6,25 @@ class Volvo240Test {
     void moveTest() {
         Volvo240 bil = new Volvo240();
         bil.gas(1);
+        bil.turnRight();
         bil.move();
-        assertEquals(1.25, bil.yPos);
+        assertEquals(1.25, bil.xPos);
+    }
+
+    @Test
+    void brakeException() {
+        Volvo240 bil = new Volvo240();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> bil.brake(10),
+                "Expected gas() to throw, but it didn't");
+        assertEquals("Argument must be in the interval [0,1].", exception.getMessage());
     }
 
     @Test
     void turnLeftTest() {
         Volvo240 bil = new Volvo240();
         bil.turnLeft();
-        assertEquals(1, bil.direction);
+        bil.turnLeft();
+        assertEquals(2, bil.direction);
     }
 
     @Test
