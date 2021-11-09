2198ccc168b329516677d3de0c70c8f0bfeda7d4
RichardSvensson <ricsven@student.chalmers.se>
2021-11-09 12:59:41 +0100

    Add test so that we fullfil 100% line coverage.

--- src/test/java/Saab95Test.java
+++ src/test/java/Saab95Test.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -15,6 +18,36 @@ class Saab95Test {
         assertEquals(1.25, bil.yPos);
     }
 
+    @Test
+    void breakTest() {
+        Saab95 bil = new Saab95();
+        bil.gas(1);
+        bil.brake(0);
+        assertEquals(1.25, bil.getCurrentSpeed());
+    }
+
+    @Test
+    void doorTest() {
+        Saab95 bil = new Saab95();
+        assertEquals(2, bil.getNrDoors());
+    }
+    @Test
+    void colorTest() {
+        Saab95 bil = new Saab95();
+        bil.setColor(Color.BLACK);
+        assertEquals(Color.BLACK, bil.getColor());
+    }
+    @Test
+    void startEngineTest() {
+        Saab95 bil = new Saab95();
+        bil.startEngine();
+        assertEquals(0.1, bil.getCurrentSpeed());
+    }
+    @Test
+    void enginePowerTest() {
+        Saab95 bil = new Saab95();
+        assertEquals(125, bil.getEnginePower());
+    }
     @Test
     void turnLeftTest() {
         Saab95 bil = new Saab95();
--- src/test/java/Volvo240Test.java
+++ src/test/java/Volvo240Test.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Volvo240Test {
     @Test
-    void moveTest() {
+    void moveEastTest() {
         Volvo240 bil = new Volvo240();
         bil.gas(1);
         bil.turnRight();
@@ -12,9 +12,43 @@ class Volvo240Test {
     }
 
     @Test
+    void moveSouthTest() {
+        Volvo240 bil = new Volvo240();
+        bil.gas(1);
+        bil.turnRight();
+        bil.turnRight();
+        bil.move();
+        assertEquals(-1.25, bil.yPos);
+    }
+
+    @Test
+    void moveWestTest() {
+        Volvo240 bil = new Volvo240();
+        bil.gas(1);
+        bil.turnLeft();
+        bil.move();
+        assertEquals(-1.25, bil.xPos);
+    }
+    @Test
+    void breakTest() {
+        Volvo240 bil = new Volvo240();
+        bil.gas(1);
+        bil.brake(1);
+        assertEquals(0, bil.currentSpeed);
+    }
+
+    @Test
     void brakeException() {
         Volvo240 bil = new Volvo240();
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> bil.brake(10),
+                "Expected break() to throw, but it didn't");
+        assertEquals("Argument must be in the interval [0,1].", exception.getMessage());
+    }
+
+    @Test
+    void gasException() {
+        Volvo240 bil = new Volvo240();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> bil.gas(10),
                 "Expected gas() to throw, but it didn't");
         assertEquals("Argument must be in the interval [0,1].", exception.getMessage());
     }
